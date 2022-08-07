6f74eed391363176c9526576b84537df6fd5eb71
Briana McCloud <bmccloud26122@gmail.com>
2022-08-07 19:12:39 -0400

    commit tree bc0901433f05d248ea10fd92f3f3ae3188d9fdaa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+//const names = ["Charlie", "Samip", "Ali"]; 
+
+function writeCards(names, event) {
+  const cards = []
+  for (let i = 0; i < names.length; i++) {
+      cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      
+
+    }
+    return cards;
+}
+
+function countDown(countDown) {
+ // let countDown = 10
+  while (countDown >= 0) {
+
+    console.log(countDown--);
+  }
+}
